19967452d10cf62f665708101bd2fff76b383de5
liujilong <18331508618@163.com>
2018-09-11 19:31:45 +0800

    first change

--- smallTools/utils.js
+++ smallTools/utils.js
@@ -59,8 +59,18 @@
       let obj = new Object();
       let search = location.search;
       let para = search.substring(search.indexOf('?') + 1);
-      let p_arr = para.split('=');
-      obj[p_arr[0]] = p_arr[1];
+      if(para !== '') {
+        if(para.indexOf('=') && para.indexOf('&')) {
+          let p_list = para.split('&');
+          p_list.forEach((item) => {
+             let p_arr = item.split('=');
+             obj[p_arr[0]] = p_arr[1];
+          })
+        }else {
+          let p_arr = para.split('=');
+          obj[p_arr[0]] = p_arr[1];
+        }
+      }
       return obj;
     },
     toUcode(str) {
@@ -88,41 +98,6 @@
       }
       // console.log(code);
       return code;
-    },
-    hCookie() {
-      return {
-        setCookie(name, value, expires) {
-          let time = Date.now();
-          let date = '';
-          if (time > 0) {
-            time = time + expires * 24 * 60 * 60 * 1000 //expires is day
-            date = new Date(time).toUTCString(); // UTC时间格式字符串
-          }
-          document.cookie = [
-            name + '=' + value,
-            expires ? 'expires' + '=' + date : '',
-            'path=/'
-          ].join(';');
-        },
-        getCookie(name) {
-          let cookies = document.cookie;
-          let value = '';
-          if (cookies.indexOf(name) < 0) {
-            return null;
-          } else {
-            (cookies.split(';') || []).map((item) => {
-              let res = item.split('=');
-              if (res[0].replace(/\s*/g, '') === name) {
-                value = res[1];
-              }
-            })
-          }
-          return value;
-        },
-        removeCookie(name) {
-          this.setCookie(name, '', -1);
-        }
-      }
     }
   }
 })(window);
